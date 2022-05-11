ec258bfcbf4ce0ae76448bc7980f75da2d960360
Zamula <dan@danbrum0.onmicrosoft.com>
2022-05-11 02:53:26 -0400

    improved debug output

--- A59/testfeed2.php
+++ A59/testfeed2.php
@@ -1,15 +1,18 @@
 <?php
-    /*
-    Template Name: TestFeed-2-A
+/*
+    Template Name: TestFeed-2
     */
 ?>
 <pre>
 <?php
-$data_source_url = 'https://padistrict42.org/wp-admin/admin-ajax.php?action=meetings&key=854aec833cc75bb085e64a72b899a2bb';
-$response = wp_remote_get($data_source_url, ['timeout' => 30, 'sslverify' => false,]);
-if (is_array($response) && !empty($response['body']) && ($body = json_decode($response['body'], true))) {
- var_export($body);
-}
-else {
-  var_export(json_last_error());
+$url = 'https://padistrict42.org/wp-admin/admin-ajax.php?action=meetings&key=854aec833cc75bb085e64a72b899a2bb';
+$response = wp_remote_get($url, ['timeout' => 30, 'sslverify' => false]);
+$body = $response['body'];
+echo ("RAW:\n\n");
+echo ($body);
+if (is_array($response) && !empty($body) && ($json = json_decode($body, true, 512, JSON_INVALID_UTF8_IGNORE))) {
+  echo ("\n\n\nJSON:\n\n");
+  echo (json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_INVALID_UTF8_IGNORE));
+} else {
+  echo (json_last_error());
 }
