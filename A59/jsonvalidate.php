fd44382569588ce40ff5212f52969de59a27b440
Zamula <dan@danbrum0.onmicrosoft.com>
2022-06-26 22:51:43 -0400

    proxy error logic

--- A59/jsonvalidate.php
+++ A59/jsonvalidate.php
@@ -3,35 +3,66 @@
     Template Name: JSON-Validate
 */
 
-$noproxy = !str_contains(urldecode($_SERVER['QUERY_STRING']), 'proxy=1');
-$qs = preg_replace('/^\s*url=(.+)\s*$/i', '\1', urldecode($_SERVER['QUERY_STRING']));
-if ($qs != 'url=') $url = esc_url_raw($qs, ['http', 'https']);
+$proxy = $_GET['proxy'];
+$ipprx = $_GET['ipprx'];
+$qs = preg_replace('/[\?&]*(?:proxy=.{0,3}|ipprx=.{0,22})(?:&|$)/i', '', urldecode($_SERVER['QUERY_STRING']));
+$url = preg_replace('/[\?&]*url=(.+)/i', '\1', $qs);
 if (preg_match('/^\s*http.{1,5}\/\/[a-z0-9]{0,10}.?area59aa\.org/i', $url)) $url = '';
 $count = ($url != '') ? 0 : -1;
 
-$ipprx = '51.81.32.81:8888';
-$ipsrc = file_get_contents('https://ipecho.net/plain', false) . ($noproxy ? '' : '&nbsp; ➜ &nbsp;' . $ipprx);
+$ipsrc = file_get_contents('https://ipecho.net/plain', false) . ($proxy == '1' ? '&nbsp; ➜ &nbsp;' . $ipprx : '');
 $ipdst = gethostbyname(parse_url($url, PHP_URL_HOST));
-$ipinfo = ' | &nbsp;&nbsp;&nbsp;&nbsp;'  . $ipsrc . '&nbsp; ➜ &nbsp;' . $ipdst;
+$ipinfo = ' | &nbsp;&nbsp;&nbsp;'  . $ipsrc . '&nbsp; ➜ &nbsp;' . $ipdst;
 
 if ($url != '') {
-  if ($noproxy) {
+  if ($proxy != '1') {
     $resp = wp_remote_get($url, ['timeout' => 30, 'sslverify' => false, 'httpversion' => '1.1']);
     $rc = wp_remote_retrieve_response_code($resp);
+    if (!is_wp_error($resp)) {
+      if (!is_array($resp)) $resp = ['error' => '', 'body' => $resp];
+      if ($rc == '200' || $rc == '301' || $rc == '302') $rc = '200';
+      else {
+        $resp['error'] = print_r($resp['body'], true);
+        if (empty($resp['http_response'])) $resp['http_response'] = $resp['body'];
+      }
+    }
   } else {
-    $respstr = file_get_contents($url, false, stream_context_create(['http' => ['proxy' => $ipprx, 'ignore_errors' => false]]));
-    $rc = $respstr === false ? '-1' : '200';
-    $resp = ['header' => $http_response_header, 'body' => $respstr];
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FAILONERROR, true);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+    if ($ipprx != '') curl_setopt($ch, CURLOPT_PROXY, $ipprx);
+    $resp = ['error' => '', 'body' => curl_exec($ch)];
+    $rc = curl_errno($ch);
+    if ($rc == '0') $rc = '200';
+    else {
+      $msg = curl_error($ch);
+      $resp['error']  = 'cURL: ' . $msg;
+      if (empty($resp['http_response'])) $resp['http_response'] = $resp;
+      if ($rc == '22') $rc = substr($msg, -3);
+    }
+    curl_close($ch);
   }
+
   // general error
-  if (is_wp_error($resp) || $rc == '-11') {
+  if (is_wp_error($resp)) {
     $msg = "ERROR response from feed. | " . print_r($resp->get_error_message(), true);
     $err = text_clean(print_r($resp, true));
   }
-  // empty body or incomplete response
-  else if (!is_array($resp) || empty($resp['body'])) {
-    $msg = 'INVALID response returned by feed.';
-    $err = text_clean(print_r($resp, true));
+  // specific http errors
+  else if ($rc == '404') {
+    $msg = 'ERROR 404 | Page not found.';
+    $err = text_clean(print_r($resp['http_response'], true));
+  } else if ($rc == '401') {
+    $msg = 'ERROR 401 | Unauthorized.';
+    $err = text_clean(print_r($resp['http_response'], true));
+  }
+  // other http errors
+  else if ($rc != '200') {
+    $msg = 'ERROR ' . $rc . ' | ' . text_clean(print_r($resp['error'], true));
+    $err = text_clean(print_r($resp['http_response'], true));
   }
   // google sheet
   else if (preg_match('/google.+export.+format=csv/i', $url) && ($json = csv_json($resp['body']))) {
@@ -50,18 +81,6 @@ if ($url != '') {
       $err = text_clean(print_r($resp['body'], true), true);
     }
   }
-  // specific http errors
-  else if ($rc == '404') {
-    $msg = 'ERROR 404 | Page not found.';
-  } else if ($rc == '401') {
-    $msg = 'ERROR 401 | Unauthorized.';
-  } else if ($rc == '-1') {
-    $msg = 'ERROR -1 | Unable to retrieve feed.';
-  }
-  // other http errors
-  else if ($rc != '200' && $rc != '301' && $rc != '302') {
-    $msg = 'ERROR ' . $rc . ' | ' . $body['error'];
-  }
   // no JSON found
   else if (substr($resp['body'], 0, 2) != '[{') {
     $msg = 'INVALID data format | No JSON data found.';
@@ -76,7 +95,7 @@ if ($url != '') {
     // invalid JSON data format
     else {
       $msg = 'ERROR parsing feed data.  ' . print_r($json['error'], true);
-      $err = text_clean(print_r($resp, true));
+      $err = text_clean(print_r($resp['body'], true));
     }
   }
   // JSON parse error
@@ -119,10 +138,21 @@ function text_clean($s, $full = false)
   <link rel="stylesheet" href="https://meetingguide.org/css/app.css">
   <link rel="mask-icon" href="https://meetingguide.org/img/meeting-guide-favicon.png" color="#00437c">
   <link rel="icon" type="image/png" href="https://meetingguide.org/img/meeting-guide-favicon.png">
-  <style type="text/ccc">aside {display:none !important;}</style>
+  <style type="text/css">
+    aside {
+      display: none !important;
+    }
+  </style>
   <script>
     window.onload = function() {
       history.pushState({}, null, unescape(location.href));
+    }
+
+    function proxyClick() {
+      chk = document.getElementById('proxy').checked;
+      document.getElementById('divProxy').style.display = chk ? "block" : "none";
+      document.getElementById('proxy').value = chk ? "1" : "";
+      if (!chk) document.getElementById('ipprx').value = "";
     }
   </script>
 </head>
@@ -148,6 +178,16 @@ function text_clean($s, $full = false)
               <input type="url" name="url" class="form-control" value="<?php echo $url; ?>" placeholder="https://distirctwebsite.org/jsonfeed/">
               <div class="input-group-append">
                 <input type="submit" class="btn btn-outline-secondary" value="Check Feed">
+              </div>
+              <div class="input-group-append" style="margin-left:10px; width:75px">
+                <span style="display:inline-block; padding:7px 5px 0 0">Proxy:</span>
+                <input type="checkbox" id="proxy" name="proxy" onclick="proxyClick();" class="form-control" <?php echo $proxy == 1 ? "checked" : "";  ?> value="<?php echo $proxy; ?>" placeholder="ipaddr:port">
+              </div>
+            </div>
+            <div id="divProxy" style="margin:10px 210px 75px; display:<?php echo $proxy == 1 ? 'block' : 'none'; ?>;">
+              <div style="float:right;" class="input-group-append">
+                <span style="display:inline-block; padding:7px 7px">Proxy Server:</span>
+                <input style="display:inline-block; width:185px;" type=" text" id="ipprx" name="ipprx" class="form-control" value="<?php echo $ipprx; ?>" placeholder="ipaddr:port">
               </div>
             </div>
           </form>
